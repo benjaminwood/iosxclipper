52729ca16a96e937838ff59e71622a7762ce0f78
Benjamin Wood <benjaminwood@gmail.com>
2018-09-23 00:29:14 -0700

    store data in temp file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const fileUpload = require("express-fileupload")
+const tempy = require('tempy')
 const { exec } = require('child_process')
 const app = express()
 
@@ -9,15 +10,16 @@ app.post('/upload', function(req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.')
 
-  let dataFile = req.files.dataFile
+  const dataFile = req.files.dataFile
+  tmpFile = tempy.file()
 
-  dataFile.mv('data', function(err) {
+  dataFile.mv(tmpFile, function(err) {
     if (err)
       return res.status(500).send(err)
     res.send()
   })
 
-  exec(`xclip -selection clipboard -i data -t ${dataFile.mimetype}`, (err, stdout, stderr) => {
+  exec(`xclip -selection clipboard -i ${tmpFile} -t ${dataFile.mimetype}`, (err, stdout, stderr) => {
     if (err) {
       console.log(err)
       return
